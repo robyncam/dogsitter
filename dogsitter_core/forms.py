e4dffad4a7ae55e28f2245d1347e8adf1878e46c
robyncam <robyncameron7@gmail.com>
2022-07-10 11:54:38 -0600

    created a login page, changed homepage to link to login page, created a link to register

--- dogsitter_core/forms.py
+++ dogsitter_core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegisterForm(forms.ModelForm):
@@ -16,5 +17,19 @@ class RegisterForm(forms.ModelForm):
             raise forms.ValidationError({"confirm_password": "Passwords do not match"})
 
         return cleaned_data
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['location', 'cost', 'is_dog_sitter', 'bio']
+
+
+class LoginForm(forms.ModelForm):
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username']
 
 
--- dogsitter_core/views.py
+++ dogsitter_core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, ProfileForm, LoginForm
 
 # Create your views here.
 
@@ -22,3 +22,37 @@ def register(request):
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, "Credientials do not match, try again")
+            return redirect('login')
+
+    context = {"form": form}
+    return render(request, 'login.html', context)
+
+
+def profile(request):
+    form = ProfileForm()
+    if request.method == "POST":
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'profile.html', context)
+
