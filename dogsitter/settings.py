5a52de76cdc71063c60154929a6a0e49beec8722
robyncam <robyncameron7@gmail.com>
2022-08-16 12:59:01 -0600

    set up postgres

--- dogsitter/settings.py
+++ dogsitter/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'dogsitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dogsitter',
+        'USER': 'dogsitter_user',
+        'PASSWORD': 'abcdef',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
