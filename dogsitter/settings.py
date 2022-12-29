44f3d00bcefe6458422489ec158e52eb0830d766
robyncam <robyncameron7@gmail.com>
2022-12-29 16:23:52 -0500

    changed deploy conf

--- dogsitter/settings.py
+++ dogsitter/settings.py
@@ -136,11 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
 )
 
 # Default primary key field type
